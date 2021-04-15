554658cb114663ab5bed3789ae1dcfd6124e37ea
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-04-15 16:53:22 +0000

    Making doc_gov_header

---
+++ doc_gov_header/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Document Gov Header",
+
+    'summary': """
+        This module add a header with standard government header style
+        """,
+
+    'author': "Kevin Jiménez, OPTIC",
+    'website': "https://optic.gob.do",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/templates.xml',
+    ],
+}
