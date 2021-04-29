2ef00efcaf10409862f38d2b9102398916b77a09
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-04-29 08:07:02 +0000

    Adding header field and images

---
+++ doc_gov_header/models/res_company.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    l10n_do_gov_header_center_data = fields.Text("Header center data")
+    l10n_do_gov_header_center_image = fields.Binary("Header Center Image")
+    l10n_do_gov_header_right_image = fields.Binary("Header Right Image")
