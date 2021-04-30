410d9128e9926aeedd302274fb990d4b37f11963
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-04-30 01:47:37 +0000

    Moving doc wh footer to a new autoinstall module

---
+++ doc_gov_wh_footer/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Document Gov WH Footer",
+
+    'summary': """
+        This module add a header with standard government footer style with approbals fields
+    """,
+
+    'author': "Kevin Jiménez, OPTIC",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'stock'],
+
+    'data': [
+        'views/templates.xml',
+    ],
+    'auto_install': True,
+}
