6800403aa01df31ee682359919192f5d10612871
José López <joselopezg8@gmail.com>
2021-05-03 08:36:07 -0400

    [ADD] stock_picking_show_qty: Show qty on printed version before validation (#10)
    * [ADD] added new module stock_picking_show_qty
    
    * [REF] stock_picking_show_qty: flake8

---
+++ stock_picking_show_qty/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Stock Picking Show Qty",
+    "summary": """
+    Show quantity on picking operation slip even if in draft
+    """,
+    "author": "José López, OPTIC",
+    "website": "https://optic.gob.do",
+    "category": "Stock",
+    "version": "14.0.0.1.0",
+    "depends": ["stock"],
+    "data": [
+        "report/report_stockpicking_operations.xml",
+    ],
+}
