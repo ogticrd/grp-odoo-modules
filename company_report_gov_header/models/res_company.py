506049a68839746cc9ccf2e3120637e492006695
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-05-17 18:39:33 +0000

    [REF] Adding gob styles to boxed layout document

--- company_report_gov_header/models/res_company.py
+++ company_report_gov_header/models/res_company.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    l10n_do_gov_header_center_data = fields.Text("Header center data")
+    l10n_do_gov_header_center_data = fields.Html("Header center data")
     l10n_do_gov_header_center_image = fields.Binary("Header Center Image")
     l10n_do_gov_header_right_image = fields.Binary("Header Right Image")
