c8d4fee57906c58ffb555a77bd2d7f01af89282e
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-06-04 11:36:58 -0400

    [ADD] Adding fields to document layout model

--- company_report_gov_header/models/res_company.py
+++ company_report_gov_header/models/res_company.py
@@ -7,3 +7,12 @@ class ResCompany(models.Model):
     l10n_do_gov_header_center_data = fields.Html("Header center data")
     l10n_do_gov_header_center_image = fields.Binary("Header Center Image")
     l10n_do_gov_header_right_image = fields.Binary("Header Right Image")
+
+    
+class BaseDocumentLayout(models.TransientModel):
+    _inherit = 'base.document.layout'
+    
+    l10n_do_gov_header_center_data = fields.Html(related='company_id.l10n_do_gov_header_center_data', readonly=False)
+    l10n_do_gov_header_center_image = fields.Binary(related='company_id.l10n_do_gov_header_center_image', readonly=False)
+    l10n_do_gov_header_right_image = fields.Binary(related='company_id.l10n_do_gov_header_right_image', readonly=False)
+    
