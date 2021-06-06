41f55a69c856c3400a6382abe6faf5a098515169
Jose Lopez <joselopezg8@gmail.com>
2021-06-06 15:05:24 -0400

    [ADD] added new module account_invoice_gov_footer

---
+++ account_invoice_gov_footer/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Stock Picking Gov Footer",
+    "summary": """
+        This module adds approval sign lines to invoice documents
+    """,
+    "author": "José López, OPTIC",
+    "website": "https://optic.gob.do",
+    "category": "Uncategorized",
+    "version": "14.0.0.1.0",
+    "depends": ["account", "company_report_gov_header"],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "auto_install": True,
+}
