75082253d6a33351612e6de17a77fc72acc3a6d1
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-08-27 01:03:29 -0400

    [ADD] stock_rest_api: Adding rest_auth_api_key as dependency

--- stock_rest_api/__manifest__.py
+++ stock_rest_api/__manifest__.py
@@ -4,7 +4,7 @@
         Rest API for Stock Management""",
     "version": "14.0.1.0.0",
     "author": "Indexa",
-    "depends": ["base_rest", "base_rest_datamodel", "stock"],
+    "depends": ["base_rest", "base_rest_datamodel", "stock", "base_rest_auth_api_key"],
     "data": [],
     "demo": [],
     "external_dependencies": {"python": ["jsondiff"]},
