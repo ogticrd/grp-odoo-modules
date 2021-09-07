dfc6d7900261bba97fb37396345976d1beae8535
José López <joselopezg8@gmail.com>
2021-09-07 17:28:10 -0400

    [ADD] added new module purchase_order_gov_footer (#16)

---
+++ purchase_order_gov_footer/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Purchase Order Gov Footer",
+    "summary": """
+        This module adds approval sign lines to purchase documents
+    """,
+    "author": "José López, OPTIC",
+    "website": "https://optic.gob.do",
+    "category": "Uncategorized",
+    "version": "14.0.0.1.0",
+    "depends": ["purchase", "company_report_gov_header"],
+    "data": [
+        "views/purchase_order_templates.xml",
+    ],
+    "auto_install": True,
+}
