0b5305bda5aa3b48d38a965949d5bddeac8345d0
Jose Lopez <joselopezg8@gmail.com>
2021-10-03 15:55:18 -0400

    [ADD] added new module purchase_order_lines_import

---
+++ purchase_order_lines_import/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Purchase Order Lines Import",
+    "summary": """
+        Purchase Order Lines Import Module
+        """,
+    "author": "José López, OPTIC",
+    "website": "https://optic.gob.do",
+    "category": "Purchases",
+    "version": "14.0.1.0.0",
+    "depends": ["purchase"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/purchase_order_lines.xml",
+        "views/purchase_views.xml",
+    ],
+    "installable": True,
+}
