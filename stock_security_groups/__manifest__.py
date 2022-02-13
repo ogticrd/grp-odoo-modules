57ff2e3048d6de07b68338c1d65026455cc1fda6
Jose Lopez <joselopezg8@gmail.com>
2022-02-13 11:58:21 -0400

    [ADD] added new module stock_security_groups

---
+++ stock_security_groups/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Stock Security Groups",
+    "summary": """
+    Custom security groups for Stock document workflows
+    """,
+    "author": "José López, OPTIC",
+    "website": "https://optic.gob.do",
+    "version": "14.0.0.0.1",
+    "depends": ["stock"],
+    "data": [
+        "security/res_groups.xml",
+    ],
+}
