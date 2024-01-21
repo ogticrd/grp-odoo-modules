9891d804ad4466631f486656b82c7a14a54d75ae
José López <joselopezg8@gmail.com>
2024-01-21 13:23:32 -0400

    [REF] l10n_do_gov_sign_purchase: get base features from l10n_do_gov_sign_base

--- l10n_do_gov_sign_purchase/__manifest__.py
+++ l10n_do_gov_sign_purchase/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "José López, OGTIC",
     "website": "https://optic.gob.do",
     "category": "Uncategorized",
-    "version": "15.0.0.1.0",
+    "version": "15.0.1.0.0",
     "depends": ["l10n_do_gov_sign_base", "purchase"],
     "data": ["views/purchase_views.xml", "data/ir_cron_data.xml"],
 }
