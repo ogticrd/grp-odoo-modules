5ca15b0cb12c1a23c8f2cf6436be8a61f0a5db5a
José López <joselopezg8@gmail.com>
2024-01-21 13:17:49 -0400

    [ADD] l10n_do_gov_sign_expense: added new module l10n_do_gov_sign_expense

---
+++ l10n_do_gov_sign_expense/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Firmas Gov Expense",
+    "summary": """
+        Firmas Gov features for Expense
+    """,
+    "author": "José López, OGTIC",
+    "website": "https://optic.gob.do",
+    "category": "Uncategorized",
+    "version": "15.0.0.1.0",
+    "depends": ["l10n_do_gov_sign_base", "hr_expense"],
+    "data": [
+        "views/hr_expense_views.xml",
+        "data/ir_cron_data.xml",
+        "data/mail_template_data.xml",
+    ],
+}
---
+++ l10n_do_gov_sign_expense/models/__init__.py
@@ -0,0 +1,2 @@
+from . import hr_expense
+from . import l10n_do_gov_document_signing_request
